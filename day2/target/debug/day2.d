cc5d4da0b3764621a6f391cd8904e7119ae5a1b8
Chetan Gupta <chetanguptaaaa21@gmail.com>
2025-12-02 19:06:23 +0530

    removed target and added gitignore

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/Users/chetangupta/coding/projects/advent-of-code-2025/day2/target/debug/day2: /Users/chetangupta/coding/projects/advent-of-code-2025/day2/src/main.rs
--- day2/target/debug/deps/day2-f9fb1027170e5aa0.d
+++
@@ -1,5 +0,0 @@
-/Users/chetangupta/coding/projects/advent-of-code-2025/day2/target/debug/deps/day2-f9fb1027170e5aa0.d: src/main.rs
-
-/Users/chetangupta/coding/projects/advent-of-code-2025/day2/target/debug/deps/libday2-f9fb1027170e5aa0.rmeta: src/main.rs
-
-src/main.rs:
